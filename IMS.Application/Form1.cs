c2289814a3a5c96dcd5abe737671b237733d9919
CpELaboratory 216 <CpELaboratory 216@BE216-PC36>
2026-02-04 08:38:33 +0800

    search bar

--- IMS.Application/Form1.cs
+++ IMS.Application/Form1.cs
@@ -9,27 +9,24 @@ namespace IMS.Application
             InitializeComponent();
             LoadPrograms();
         }
-        private void LoadPrograms()
+        private void LoadPrograms(string? searchTerm = null)
         {
-            using (var _context = new AppDbContext())
-            {
-                dataGridView1.DataSource = _context.Programs.ToList();
-            }
-        }
-
-        private void textBoxSearch_TextChanged(object sender, EventArgs e)
-        {
-            var searchTerm = textBoxSearch.Text.ToLower();
             using (var _context = new AppDbContext())
             {
                 if (string.IsNullOrEmpty(searchTerm))
                     dataGridView1.DataSource = _context.Programs.ToList();
                 else
                     dataGridView1.DataSource = _context.Programs
-                        .Where(p => p.Name.ToLower().Contains(searchTerm) ||
+                        .Where(p => p.Id.ToString().Contains(searchTerm) ||
+                                    p.Name.ToLower().Contains(searchTerm) ||
                                     p.Description.ToLower().Contains(searchTerm))
                         .ToList();
             }
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            LoadPrograms(textBoxSearch.Text.ToLower());   
         }
     }
 }
